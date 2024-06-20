5aa48edb7636fabc0162f000c14096ff5dda943d
Mustafa Kürşad BAŞER <mkursatbaser@gmail.com>
2024-06-20 09:46:22 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleApp_SiskonAutomation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
