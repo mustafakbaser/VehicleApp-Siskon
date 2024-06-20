8174ea83ce24394222e9add3c1548ddd3845c616
Mustafa Kürşad BAŞER <mkursatbaser@gmail.com>
2024-06-20 15:21:42 +0300

    Restrictions added for BE & FE

---
+++ Models/PlateAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace VehicleApp_SiskonAutomation.Models
+{
+    public class PlateAttribute : ValidationAttribute
+    {
+        private const string PlatePattern = @"^\d{2} [A-Z]{1,3} \d{2,4}$"; // RegEx For Turkish Plate Standard
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return new ValidationResult("Plate is required.");
+            }
+
+            var plate = value.ToString();
+            if (!Regex.IsMatch(plate, PlatePattern))
+            {
+                return new ValidationResult("Plate is not in the correct format. The correct format is '35 ABC 123'.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -5,14 +5,39 @@ namespace VehicleApp_SiskonAutomation.Models
     public class Vehicle
     {
         [Key]
+        [Required]
+        [Plate]
         public string Plate { get; set; }
 
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Nickname of Vehicle")]
         public string Nickname { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Brand { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Model { get; set; }
+
+        [Required]
+        [Range(1900, 2100)]
+        [Display(Name = "Model Year")]
         public int ModelYear { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Vehicle Type")]
         public string VehicleType { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Color { get; set; }
+
+        [Required]
+        [Display(Name = "Is Active")]
         public bool IsActive { get; set; }
     }
 }
