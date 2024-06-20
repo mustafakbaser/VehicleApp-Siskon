3e2289c56c73c93591e8cc09c794acb366aef256
Mustafa Kürşad BAŞER <mkursatbaser@gmail.com>
2024-06-20 09:53:56 +0300

    Entity and DbContext created

---
+++ Data/VehicleContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleApp_SiskonAutomation.Models;
+
+namespace VehicleApp_SiskonAutomation.Data
+{
+    public class VehicleContext : DbContext
+    {
+        public VehicleContext(DbContextOptions<VehicleContext> options) : base(options) { }
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
---
+++ Models/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿namespace VehicleApp_SiskonAutomation.Models
+{
+    public class Vehicle
+    {
+        public string Plate { get; set; }
+        public string Nickname { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int ModelYear { get; set; }
+        public string VehicleType { get; set; }
+        public string Color { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
